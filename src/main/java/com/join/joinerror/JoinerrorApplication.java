e4cf280e7c85a779b02cfc8cf723d152abf26739
syr9707 <yrshin9771@gmail.com>
2023-01-17 21:20:03 +0900

    test first commit

---
+++ src/main/java/com/join/joinerror/JoinerrorApplication.java
@@ -0,0 +1,13 @@
+package com.join.joinerror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinerrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinerrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/join/joinerror/JoinerrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.join.joinerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinerrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
